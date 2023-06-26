40ef8158776cbba75cdf466368b0592a08b2ffe2
Rayean-Mahmud <rayean.mahmud.arnob@gmail.com>
2023-06-26 10:16:28 +0600

    uploading login and sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,13 @@ import 'package:project/login.dart';
 import 'package:project/register.dart';
 
 void main() {
-  runApp(MaterialApp(   //widget with more features i.e nav, themes
+  runApp(MaterialApp(
+    //widget with more features i.e nav, themes
     debugShowCheckedModeBanner: false, // remove debug banner from screen
     initialRoute: 'login', // 1st goes to login page
     routes: {
-      'login': (context) => MyLogin(),//class name in which login page is set
+      'login': (context) => MyLogin(), //class name in which login page is set
       'register': (context) => MyRegister(),
     },
   ));
 }
-
-
